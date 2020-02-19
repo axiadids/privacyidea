3d1b65ee69ea88aabcf6036f6e783c15059e534f
Darren He <dhe@axiadids.com>
2020-02-19 15:45:21 -0800

    Revert "TUTI-3158 Add otp_pin_digit_delta"
    This reverts commit 378cf197a2a61f821b0b5ed36e0dd2fe7804b36f.

--- privacyidea/lib/policy.py
+++ privacyidea/lib/policy.py
@@ -263,7 +263,6 @@ class ACTION(object):
     OTPPINMAXLEN = 'otp_pin_maxlength'
     OTPPINMINLEN = 'otp_pin_minlength'
     OTPPINCONTENTS = 'otp_pin_contents'
-    OTPPINDIGITDELTA = 'otp_pin_digit_delta'
     PASSNOTOKEN = "passOnNoToken"
     PASSNOUSER = "passOnNoUser"
     PASSTHRU = "passthru"
@@ -1481,9 +1480,6 @@ def get_static_policy_definitions(scope=None):
                                               "(c)haracters, (n)umeric, "
                                               "(s)pecial, (o)thers. [+/-]!"),
                                     'group': GROUP.PIN},
-            ACTION.OTPPINDIGITDELTA: {'type': 'bool',
-                                      "desc": _("Digit-PIN must have different deltas."),
-                                      'group': GROUP.PIN},
             ACTION.AUDIT: {'type': 'bool',
                            "desc": _("Admin is allowed to view the Audit log."),
                            "group": GROUP.SYSTEM,
@@ -1692,9 +1688,7 @@ def get_static_policy_definitions(scope=None):
                                               "(c)haracters, (n)umeric, "
                                               "(s)pecial, (o)thers. [+/-]!"),
                                     'group': GROUP.PIN},
-            ACTION.OTPPINDIGITDELTA: {'type': 'bool',
-                                      "desc": _("Digit-PIN must have different deltas."),
-                                      'group': GROUP.PIN},
+
             ACTION.AUDIT: {
                 'type': 'bool',
                 'desc': _('Allow the user to view his own token history.'),
